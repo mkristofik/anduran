72e11d54faef444a807ec892ade9f8a125337ed0
Michael Kristofik <kristo605@gmail.com>
2019-06-07 23:08:05 -0500

    Better error messages through template metaprogramming.

--- src/iterable_enum_class.h
+++ src/iterable_enum_class.h
@@ -40,21 +40,31 @@
     } \
     constexpr T operator*(T t) { return t; } \
     constexpr T begin(T) { return T::_first; } \
-    constexpr T end(T) { return T::_last; }
+    constexpr T end(T) { return T::_last; } \
+    template <> struct IsIterableEnumClass<T> : std::true_type {};
 
-// TODO: template metaprogramming to actually require an iterable enum class?
+// Learned how to make a custom type trait from here:
+// https://akrzemi1.wordpress.com/2017/12/02/your-own-type-predicate/.  The macro
+// specializes this type to be true for iterable enum classes and false for any
+// other type.  This allows for better error messages with the static_asserts
+// below.
+template <typename T>
+struct IsIterableEnumClass : std::false_type {};
+
 template <typename T>
 constexpr int enum_size()
 {
+    static_assert(IsIterableEnumClass<T>::value, "requires iterable enum class");
     using U = typename std::underlying_type_t<T>;
     return U(T::_last) - U(T::_first);
 }
 
-// Index into an array with an enum without casting.
+// Index into an array with an iterable enum without casting.  Intended for
+// arrays defined with enum_size as the template argument.  You can get away with
+// any iterable enum as long as the sizes match, but please don't.
 template <typename T, size_t N, typename U>
 constexpr const T & enum_fetch(const std::array<T, N> &ary, U enumValue)
 {
-    static_assert(std::is_enum_v<U>, "requires iterable enum class");
     static_assert(enum_size<U>() == N, "array must have same size as iterable enum");
     return ary[static_cast<int>(enumValue)];
 }
